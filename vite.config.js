539e4852e4b4231e8dc47bf4bd6905f69f0e0f9b
AbdullahZafar327 <codingbucketorg@gmail.com>
2026-06-05 09:09:14 +0500

    remove proxy server

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    proxy: {
-      '/v1': 'https://svelte-backend-xqxj.onrender.com',
-    },
-  },
 })
